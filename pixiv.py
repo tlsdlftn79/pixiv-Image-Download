1f656e2f0f10dc2c1156447745512d90de869387
Hatsune Miku <47713430+tlsdlftn79@users.noreply.github.com>
2023-07-09 16:30:45 +0900

    Update pixiv.py
    작품입력하고 이미지 다운받고 팅겨는 현상 수정

--- pixiv.py
+++ pixiv.py
@@ -1,8 +1,8 @@
-﻿import os
+import os
 from pixivpy3 import AppPixivAPI
 import sys
 
-REFRESH_TOKEN = '픽시브 api 토큰 발급'
+REFRESH_TOKEN = 'API 토큰 발급 하시고 사용'
 
 def download_image(aapi, illust_id, base_path, file_name=None):
     detail = aapi.illust_detail(illust_id)
@@ -20,12 +20,17 @@ def main():
     # Perform login
     aapi.auth(refresh_token=REFRESH_TOKEN)
 
-    # Enter the artwork ID
-    print("작품번호 입력:")
-    artwork_id = input()
+    # Continuously ask for artwork IDs and download images
+    while True:
+        print("작품번호 입력 (끝내려면 'exit' 입력):")
+        artwork_id = input()
 
-    # Download image
-    download_image(aapi, artwork_id, './images')
+        if artwork_id.lower() == 'exit':
+            break
+
+        # Download image
+        download_image(aapi, artwork_id, './images')
 
 if __name__ == '__main__':
     main()
+
